f5fb5596435049cd0e72a26ad217c6f69e7170c3
bleart selimi <bleart.selimi@outlook.com>
2023-12-30 13:32:45 +0200

    document.onreadystatechange with splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import { Grocha, Portfolio } from './pages';
 import { useEffect } from 'react';
 import { useGeneralContext } from './hooks/useGeneralContext';
 
+declare global {
+  interface Window {
+    documentReadyStateAnimationEvent: (readyStateValue?: string) => void;
+  }
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,37 +33,27 @@ const App = () => {
 
   const { dispatch } = useGeneralContext();
 
-  const documentReadyStateAnimationEvent = () => {
+  window.documentReadyStateAnimationEvent = (readyStateValue) => {
     const splashScreenElement = document.querySelector(".splash-screen") as HTMLElement | null;
     const bodyElement = document.querySelector("body") as HTMLElement | null;
-    if (!sessionStorage.getItem('dontShowSplashScreen')) {
-      if (splashScreenElement) {
-        splashScreenElement.style.opacity = "1";
-        splashScreenElement.style.visibility = "visible";
-        setTimeout(() => {
-          if (splashScreenElement && bodyElement) {
-            bodyElement.style.overflowY = 'auto';
-            splashScreenElement.style.transition = "all .3s cubic-bezier(0.645, 0.045, 0.355, 1)";
-            splashScreenElement.style.opacity = "0";
-            splashScreenElement.style.visibility = "collapse";
-            dispatch({ type: "SPLASH_SCREEN_STATE", payload: true });
-            sessionStorage.setItem('dontShowSplashScreen', 'true');
-          }
-        }, 3000)
-      }
-    }
-    else {
-      if (splashScreenElement && bodyElement) {
-        bodyElement.style.overflowY = 'auto';
-        splashScreenElement.style.opacity = "0";
-        splashScreenElement.style.visibility = "collapse";
-      }
-      dispatch({ type: "SPLASH_SCREEN_STATE", payload: true });
+    if (splashScreenElement && readyStateValue) {
+      splashScreenElement.style.opacity = "1";
+      splashScreenElement.style.visibility = "visible";
+      setTimeout(() => {
+        if (splashScreenElement && bodyElement) {
+          bodyElement.style.overflowY = 'auto';
+          splashScreenElement.style.transition = "all .3s cubic-bezier(0.645, 0.045, 0.355, 1)";
+          splashScreenElement.style.opacity = "0";
+          splashScreenElement.style.visibility = "collapse";
+          dispatch({ type: "SPLASH_SCREEN_STATE", payload: true });
+          sessionStorage.setItem('dontShowSplashScreen', 'true');
+        }
+      }, 1500)
     }
   }
 
   useEffect(() => {
-    documentReadyStateAnimationEvent();
+    window.documentReadyStateAnimationEvent();
   }, [])
 
   return (
