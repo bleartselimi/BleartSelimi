297a65c69441a56c918611ba7f0243b709f5ee28
bleart selimi <bleart.selimi@outlook.com>
2024-01-05 16:39:01 +0100

    document.body.scrollTop on transition page

--- src/components/Transition/Transition.tsx
+++ src/components/Transition/Transition.tsx
@@ -19,7 +19,7 @@ const Transition = () => {
             dispatch({ type: "GLOBAL_LOADING_STATE", payload: false });
             slideTopRef.current!.style.transform = "translateY(calc(0% - 1px))";
             setTimeout(() => {
-                document.querySelector("body")!.scrollTop = 0;
+                document.body.scrollTop = 0;
                 menuOpend(false);
                 transitionOut(true);
                 navigate(state.transitionIn.to);
